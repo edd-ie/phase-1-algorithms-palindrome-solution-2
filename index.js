d65aa18ef8f6916830dfacb12ebea92a17377d0d
edd-ie <ogendoeddie@gmail.com>
2023-03-29 14:53:05 +0300

    complete

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for(let i = 0; i < word.length/2; i++) {
+    if (word[i]===word[word.length-(i+1)]) {continue}
+    else{return false}
+  }
+  return true
 }
 
 /* 
   Add your pseudocode here
+  Loop through half the word
+    compare if the opposite letters are equal
+    return true/false depending on result
 */
 
 /*
   Add written explanation of your solution here
+  for loop iterates through the 1st half of the word
+  the if statement compares opposite letters moving closer to the middle with each loop
+  if the aren't equal it stops and returns false
+  else return true
 */
 
 // You can run `node index.js` to view these console logs
